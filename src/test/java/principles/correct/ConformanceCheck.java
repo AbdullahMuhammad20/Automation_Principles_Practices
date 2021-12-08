dd9c8a82198c7d04384c39913e887ea0f5859d06
Abdullah Mohamed <abdullah.muhammed@mondia.com>
2021-12-08 09:12:29 +0200

    add all files and conformance testing is added

--- src/test/java/principles/correct/ConformanceCheck.java
+++ src/test/java/principles/correct/ConformanceCheck.java
@@ -1,6 +1,26 @@
 package principles.correct;
 
-public class ConfrmanceCheck
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class ConformanceCheck
 {
 
+    // Conformance testing on names
+    @DataProvider
+    Object[][] nameProvider()
+    {
+        return new Object[][]
+                {
+                        {"John Smith"},
+                        {"Jose de la Torres"},
+                        {"Chris O'Reilly"}
+                };
+    }
+    @Test(dataProvider = "nameProvider")
+    public void verifyX(String s)
+    {
+        // Here you need to pass all names come from provider to the function that will handle it
+        //parseName(s);
+    }
 }
